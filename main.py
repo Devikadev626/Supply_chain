094868d52d56321f27ed06594966f6a2ce160f5d
Devika <devikadev626@gmail.com>
2025-05-15 12:50:44 +0530

    Data Validation stage completed

---
+++ main.py
@@ -0,0 +1,26 @@
+from src import logger
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
+logger.info("welcome to the Demand forecast project")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/config/configuration.py
@@ -0,0 +1,42 @@
+from src.entity.config_entity import dataingestion_config,DataValidationConfig
+from src.constants import *
+from src.utils.common import read_yaml,create_directories
+
+
+class ConfiguarationManager:
+    def __init__(
+        self,
+        config_file = config_file_path,
+        schema_file = schema_file_path,
+        params_file = params_file_path):
+
+        self.config = read_yaml(config_file)
+        self.schema = read_yaml(schema_file)
+        self.params = read_yaml(params_file)
+
+        create_directories([self.config.artifacts_root])
+
+    def get_data_ingestion_config(self)->dataingestion_config:
+        config = self.config.data_ingestion
+        create_directories([config.root_dir])
+
+        data_ingestion_config = dataingestion_config(
+            root_dir = config.root_dir,
+            source_url = config.source_url,
+            local_data_file = config.local_data_file,
+            unzip_dir = config.unzip_dir
+        )
+        return data_ingestion_config
+    
+    def get_data_validation_config(self)->DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            unzip_data_dir = config.unzip_data_dir,
+            STATUS_FILE = config.STATUS_FILE,
+            all_schema = schema
+        )
+        return data_validation_config
